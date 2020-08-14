9a03f2423901fb9277ea5e5d8ba713dee5378d20
mahmoudaboelnaga <m.aboelnaga@myonekey.com>
2020-08-14 18:45:56 +0200

    first commit

---
+++ invitation/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('landing_app.urls')),
+    path('', include('invitation_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('account.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
