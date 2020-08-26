0436d3f01807577459cd255bac7ca63787d68fc6
mahmoudaboelnaga <m.aboelnaga@myonekey.com>
2020-08-26 06:27:28 +0200

    update settings

--- invitation/settings.py
+++ invitation/settings.py
@@ -147,12 +147,11 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 LOGIN_REDIRECT_URL = '/accounts/profile'
 LOGOUT_REDIRECT_URL = '/'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
